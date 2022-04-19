d8748b7013f4e2c37e81ef8237d01924b95f21e8
sam-johnny <sam.johnny@live.fr>
2022-04-19 12:07:44 +0200

    feat: create product index

---
+++ src/Controller/ApiProductController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiProductController extends AbstractController
+{
+    private ProductRepository $repository;
+
+    public function __construct(ProductRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    #[Route('/api/product/index', name: 'app_api_product_index', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->json([
+            $this->repository->findAll(),
+            200
+        ]);
+    }
+}
