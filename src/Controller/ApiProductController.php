13c4ad9f119701a8263ff0e0f746b63f025227f5
sam-johnny <sam.johnny@live.fr>
2022-04-19 13:34:49 +0200

    feat: create product index

--- src/Controller/ApiProductController.php
+++ src/Controller/ApiProductController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Product;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
