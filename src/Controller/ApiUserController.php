94c20baece658b5f2b964a19f378b997b32433a0
sam-johnny <sam.johnny@live.fr>
2022-04-20 18:03:54 +0200

    feat: delete user

--- src/Controller/ApiUserController.php
+++ src/Controller/ApiUserController.php
@@ -18,12 +18,21 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 class ApiUserController extends AbstractController
 {
     private UserRepository $repository;
+    private EntityManagerInterface $entityManager;
 
-    public function __construct(UserRepository $repository)
+    /**
+     * @param UserRepository $repository
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(UserRepository $repository, EntityManagerInterface $entityManager)
     {
         $this->repository = $repository;
+        $this->entityManager = $entityManager;
     }
 
+    /**
+     * @return Response
+     */
     #[Route(name: 'app_api_user_collection_get', methods: ['GET'])]
     public function collection(): Response
     {
@@ -34,24 +43,34 @@ class ApiUserController extends AbstractController
             ['groups' => 'user:index']);
     }
 
+    /**
+     * @param int $id
+     * @return Response
+     */
     #[Route('/{id}', name: 'app_api_user_item_get', requirements: ['id' => '[\d]+'], methods: ['GET'])]
-    public function item(): Response
+    public function item(int $id): Response
     {
         return $this->json(
-            $this->repository->findOneBy([]),
+            $this->repository->find($id),
             Response::HTTP_OK,
             [],
             ['groups' => 'user:index']
         );
     }
 
+    /**
+     * @param SerializerInterface $serializer
+     * @param Request $request
+     * @param ValidatorInterface $validator
+     * @param UrlGeneratorInterface $urlGenerator
+     * @return Response
+     */
     #[Route(name: 'app_api_user_item_post', methods: ['POST'])]
     public function user(
-        SerializerInterface    $serializer,
-        Request                $request,
-        ValidatorInterface     $validator,
-        EntityManagerInterface $entityManager,
-        UrlGeneratorInterface  $urlGenerator
+        SerializerInterface   $serializer,
+        Request               $request,
+        ValidatorInterface    $validator,
+        UrlGeneratorInterface $urlGenerator
     ): Response
     {
         try {
@@ -64,8 +83,8 @@ class ApiUserController extends AbstractController
                 return $this->json($errors, Response::HTTP_BAD_REQUEST);
             }
             $user->setCustomer($this->getUser());
-            $entityManager->persist($user);
-            $entityManager->flush();
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
 
             return $this->json(
                 $user,
@@ -80,5 +99,22 @@ class ApiUserController extends AbstractController
                 'message' => $e->getMessage()
             ], Response::HTTP_BAD_REQUEST);
         }
+    }
+
+    /**
+     * @param $id
+     * @return Response
+     */
+    #[Route('/{id}', name: 'app_api_user_item_delete', methods: ['DELETE'])]
+    public function delete($id): Response
+    {
+        $user = $this->repository->find($id);
+        $this->entityManager->remove($user);
+        $this->entityManager->flush();
+
+        return $this->json(
+            null,
+            Response::HTTP_NO_CONTENT
+        );
     }
 }
